07a0498bbd45e47bdce3b2694d684eb931632d97
Oladayo <oladayoafolabi@outlook.com>
2025-02-26 03:30:17 +0000

    constructor created

--- lib/ui /MoviesListViewDetails.dart
+++ lib/ui /MoviesListViewDetails.dart
@@ -1,7 +1,5 @@
-
 // NEW ROUTE OR PAGE
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 
 import 'package:listof_view_movies/model/movie.dart';
 
@@ -9,35 +7,31 @@ class MovieListViewDetails extends StatelessWidget {
   final String movieName;
   final Movie movie;
 
-  const MovieListViewDetails({super.key, required this.movieName, required this.movie});
-
- 
+  const MovieListViewDetails(
+      {super.key, required this.movieName, required this.movie});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:  Text("Movies", style: mainTextStyle()),
+        title: Text("Movies", style: mainTextStyle()),
         centerTitle: false,
         backgroundColor: Colors.blueGrey.shade900,
-        ),
+      ),
       body: ListView(
-        children:  [
-         MovieDetailsThumbnail(thumbnail: movie.images[0]),
-         MovieDetailsHeaderWithPoster(movie: movie)
-
-      ],),
+        children: [
+          MovieDetailsThumbnail(thumbnail: movie.images[0]),
+          MovieDetailsHeaderWithPoster(movie: movie)
+        ],
+      ),
     );
   }
-  
-  TextStyle mainTextStyle () {
-     return const TextStyle(   
-      fontWeight: FontWeight.bold, 
-    color: Color.fromARGB(255, 255, 255, 255));
+
+  TextStyle mainTextStyle() {
+    return const TextStyle(
+        fontWeight: FontWeight.bold, color: Color.fromARGB(255, 255, 255, 255));
   }
 }
-
-
 
 class MovieDetailsThumbnail extends StatelessWidget {
   final String thumbnail;
@@ -57,29 +51,28 @@ class MovieDetailsThumbnail extends StatelessWidget {
             Container(
               width: MediaQuery.of(context).size.width,
               height: 190,
-              decoration:BoxDecoration(
-                image: DecorationImage(image: NetworkImage(thumbnail),fit: BoxFit.cover)
-              ),
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: NetworkImage(thumbnail), fit: BoxFit.cover)),
             ),
-            const Icon(Icons.play_circle_outline, color: Colors.white, size:100)
+            const Icon(Icons.play_circle_outline,
+                color: Colors.white, size: 100)
           ],
         ),
         Container(
           decoration: const BoxDecoration(
-            gradient: LinearGradient(colors: [Color(0x00f5f5f5),Color(0xfff5f5f5)],
-            
+              gradient: LinearGradient(
+            colors: [Color(0x00f5f5f5), Color(0xfff5f5f5)],
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
-            
-            )
-          ),
+          )),
           height: 80,
         )
-
       ],
     );
   }
 }
+
 class MovieDetailsHeaderWithPoster extends StatelessWidget {
   final Movie movie;
   const MovieDetailsHeaderWithPoster({
@@ -89,21 +82,20 @@ class MovieDetailsHeaderWithPoster extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Padding(
-      padding: const EdgeInsets.symmetric(horizontal:16),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
       child: Row(
         children: [
           MoviePoster(Poster: movie.images[0].toString()),
-          const SizedBox(width: 16.0,),
-          Expanded
-          (
-            child: MovieDetailsHeader(movie: movie))
+          const SizedBox(
+            width: 16.0,
+          ),
+          Expanded(child: MovieDetailsHeader(movie: movie))
         ],
       ),
     );
   }
 }
-
 
 class MoviePoster extends StatelessWidget {
   final String Poster;
@@ -116,19 +108,21 @@ class MoviePoster extends StatelessWidget {
   Widget build(BuildContext context) {
     var borderRadius = const BorderRadius.all(Radius.circular(10));
     return Card(
-      child: ClipRRect(borderRadius: borderRadius,
-      child: Container(
-            width: MediaQuery.of(context).size.width/4,
-            height: 160,
-            decoration: BoxDecoration(
-              image: DecorationImage(image: NetworkImage(Poster),fit: BoxFit.cover),
-            
-            ),
-      ),
+      child: ClipRRect(
+        borderRadius: borderRadius,
+        child: Container(
+          width: MediaQuery.of(context).size.width / 4,
+          height: 160,
+          decoration: BoxDecoration(
+            image:
+                DecorationImage(image: NetworkImage(Poster), fit: BoxFit.cover),
+          ),
+        ),
       ),
     );
   }
 }
+
 class MovieDetailsHeader extends StatelessWidget {
   final Movie movie;
   const MovieDetailsHeader({super.key, required this.movie});
@@ -138,37 +132,26 @@ class MovieDetailsHeader extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text("${movie.year}. ${movie.genre}".toUpperCase(),style: const TextStyle(
-          fontWeight: FontWeight.w400,
-          color: Colors.cyan
-        ),),
-        Text(movie.title, style: const TextStyle(
-          fontWeight: FontWeight.w500,
-          fontSize: 32
-        ),),
-       Text.rich(
-          TextSpan(
-            style: const TextStyle(
-          fontSize: 12,
-          fontWeight: FontWeight.w300
+        Text(
+          "${movie.year}. ${movie.genre}".toUpperCase(),
+          style:
+              const TextStyle(fontWeight: FontWeight.w400, color: Colors.cyan),
         ),
-        children: <TextSpan>[
-           TextSpan(
-            text: movie.plot
-           ),
-           const TextSpan(
-            text: "More...",
-            style: TextStyle(
-              color: Colors.indigoAccent
-            )
-           )
-
-        ]),
+        Text(
+          movie.title,
+          style: const TextStyle(fontWeight: FontWeight.w500, fontSize: 32),
+        ),
+        Text.rich(
+          TextSpan(
+              style: const TextStyle(fontSize: 12, fontWeight: FontWeight.w300),
+              children: <TextSpan>[
+                TextSpan(text: movie.plot),
+                const TextSpan(
+                    text: "More...",
+                    style: TextStyle(color: Colors.indigoAccent))
+              ]),
         )
-      
-      ]
-      ,
-
+      ],
     );
   }
 }
